03dacaf0af4200f089b502adaaf98cb0b1ab556b
aramirez22itm <alexanderramirez1131113@correo.itm.edu.co>
2026-03-05 19:59:23 -0500

    Add AuditBase entity

---
+++ InventorySystem.Domain/Entities/AuditBase.cs
@@ -0,0 +1,8 @@
+﻿namespace InventorySystem.Domain.Entities;
+
+public abstract class AuditBase
+{
+    public int Id { get; set; } // Llave primaria [cite: 122, 1176]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Fecha de creación [cite: 123, 1177]
+    public DateTime? UpdatedAt { get; set; } // Fecha de actualización (opcional) [cite: 124, 1178]
+}
---
+++ InventorySystem.Domain/Entities/Warehouse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace InventorySystem.Domain.Entities;
+
+[cite_start]
+public class Warehouse : AuditBase // Hereda Id y fechas de AuditBase 
+{
+    public string Name { get; set; } = string.Empty; // Nombre del almacén [cite: 134, 1188]
+    public string Address { get; set; } = string.Empty; // Dirección
+    public int Capacity { get; set; } // Capacidad de almacenamiento
+}
